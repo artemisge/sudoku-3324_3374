5c0ee5fbbca839e7730564b9a6936ba512b7b297
Oxi reman KALA <cpozriki@csd.auth.gr>
2019-12-14 20:44:31 +0200

    xrysa

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import static java.awt.Color.CYAN;
 
 public class GUI extends JFrame
 {
@@ -15,7 +21,50 @@ public class GUI extends JFrame
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
+        setForeground(CYAN);
+
+        JMenuBar menu=new JMenuBar();
+
+        JMenu menuOptions=new JMenu("Options");
+        menuOptions.setMnemonic(KeyEvent.VK_A);
+        menuOptions.setDisplayedMnemonicIndex(0);
+        menu.add(menuOptions);
+
+        JMenu newGame =new JMenu("New Game");
+        JMenuItem classic=new JMenuItem("Classic");
+        JMenuItem killer=new JMenuItem("Killer");
+        JMenuItem duidoku=new JMenuItem("Duidoku");
+        newGame.add(classic);
+        newGame.add(killer);
+        newGame.add(duidoku);
+        menuOptions.add(newGame);
+
+        menuOptions.addSeparator();
+
+        JMenuItem logIn=new JMenuItem("Log in");
+        logIn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //popup to log in
+            }
+        });
+        menuOptions.add(logIn);
+
+        menuOptions.addSeparator();
+
+        JMenuItem signIn=new JMenuItem("Sign in");
+        signIn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //popup to sign in
+            }
+        });
+        menuOptions.add(signIn);
+
+        setJMenuBar(menu);
+
         setVisible(true);
+
     }
 
 
