c1a0e8b93101dd1b4325ed83444ab4aee1842ba2
Oxi reman KALA <cpozriki@csd.auth.gr>
2019-12-14 19:10:39 +0200

    xrysa

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,22 @@
-public class GUI
+import javax.swing.*;
+
+public class GUI extends JFrame
 {
+
+    public GUI()
+    {
+        makeFrame();
+    }
+
+    private void makeFrame()
+    {
+        setTitle("Sudoku");
+        setSize(600, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
-    public static final int PUZZLES = 10;
-
-    public static void main(String[] args) {
-        System.out.println("Hello, let's play Sudoku!");
-        SudokuFrame frame = new SudokuFrame();
+    public static void main(String[] args)
+    {
+        GUI myGui=new GUI();
     }
 }
